312e65e27ad5e6bd8981b940b0cbfb36e5110ed6
harsh1947-jain <harsh1947jain@gmail.com>
2026-04-02 15:09:33 +0530

    Added subtraction also

--- addition.py
+++ addition.py
@@ -1,19 +1,20 @@
-def add_two_numbers(a, b):
+def add_three_numbers(a, b, c):
     """
-    Function to add two numbers.
+    Function to add three numbers.
     
     Args:
         a (int or float): First number
         b (int or float): Second number
-    
+        c (int or float): Third number
     Returns:
-        int or float: Sum of a and b
+        int or float: Sum of a,b,and c
     """
-    return a + b
+    return a + b + c
 
 # Example usage
 if __name__ == "__main__":
     num1 = 5
     num2 = 3
-    result = add_two_numbers(num1, num2)
-    print(f"The sum of {num1} and {num2} is {result}")
+    num3 = 4
+    result = add_three_numbers(num1, num2, num3)
+    print(f"The sum of {num1}, {num2}, and {num3} is {result}")
---
+++ subtraction.py
@@ -0,0 +1,21 @@
+def subtract_two_numbers(a, b):
+    """
+    Function to subtract two numbers.
+    
+    Args:
+        a (int or float): First number
+        b (int or float): Second number
+    Returns:
+        int or float: Difference of a and b
+    """
+    return a - b
+
+
+if __name__ == "__main__":
+    num1 = 5
+    num2 = 3
+    
+    result = subtract_two_numbers(num1, num2)
+    print(f"The difference of {num1} and {num2} is {result}")
+
+    print("hello rohit")
